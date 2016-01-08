78b979849a259ef2fc78f831bc54114476fd6961
liuwencai <821272852@qq.com>
2016-01-08 23:32:56 +0800

    添加了几个表sql

---
+++ dbsource/filldata.sql
@@ -0,0 +1,3 @@
+insert into user (phone,password,nickname) values('1340713890','123456','liuwencai');
+
+insert into goods(goodsname,goods_ecode,price,pic,shopid,last_refeash_user) value('水杯','3','23','sssss','1','2');
---
+++ dbsource/zhongcha.sql
@@ -0,0 +1,59 @@
+-- ----------------------------
+--  Table structure for `user`
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` text NOT NULL,
+  `password` text,
+  `nickname` text,
+  PRIMARY KEY (`uid`)
+) ENGINE=MyISAM AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+-- ----------------------------
+--  Table structure for 'goods'
+-- ----------------------------
+DROP TABLE IF EXISTS `goods`;
+CREATE TABLE `goods` (
+  `goodsid` int(11) NOT NULL AUTO_INCREMENT,
+  `goods_ecode` text,
+  `goodsname` text,
+  `price` decimal(5,2) DEFAULT NULL,
+  `pic` text DEFAULT NULL,
+  `shopid` int(11) NOT NULL, 
+  'classid' int(11) NOT NULL,
+  `last_rfeash_time` timestamp DEFAULT CURRENT_TIMESTAMP,
+  `last_refeash_user` int(11) NOT NULL,
+  PRIMARY KEY(`goodsid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- ---------------------------
+-- Table shop
+-- ---------------------------
+DROP TABLE IF EXISTS `shop`;
+CREATE TABLE `shop` (
+  `shopid` int(11) NOT NULL AUTO_INCREMENT,
+  `shopname` text,
+  `pic` text DEFAULT NULL,
+  `longitude` double(10,3) NOT NULL,
+  `latitude` double(10,3) NOT NULL,
+  `jointime` timestamp DEFAULT CURRENT_TIMESTAMP,
+ PRIMARY KEY (`shopid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- ---------------------------
+-- Tables structure for unit
+-- ---------------------------
+DROP TABLE IF EXISTS `unit`
+CREATE TABLE `unit` (
+  `unitid` int(11) NOT NULL AUTO_INCREMENT,
+  `unitname` text,
+  PRIMARY KEY(`unitid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+-- ---------------------------
+-- Table for class
+-- ---------------------------
+CREATE TABLE `class` (
+  `classid` int(11) NOT NULL AUTO_INCREMENT,
+  `fatherid` int(11),
+  `classname` text,
+  `unitid` int(11) NOT NULL,
+  PRIMARY KEY(`classid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
