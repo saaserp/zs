994b5f654e4befd1abc5648a56f1ad8166ce4627
liuwencai <821272852@qq.com>
2016-01-10 14:58:03 +0800

    add some code

--- src/service/GetSimiarGoodsInNearShopProcesser.java
+++ src/service/GetSimiarGoodsInNearShopProcesser.java
@@ -15,12 +15,11 @@ public class GetSimiarGoodsInNearShopProcesser extends BaseDao implements IProce
 		String goodsName=map.get("goodsName");
 		String currentShopID=map.get("currentShopID");
 		
-		String sql="select goodsid as goodsID," +
-				"goodsname as goodsName," +
-				"price,unit from ";
+		String sql="select   goodsid as goodsID,goodsname as goodsName,price,unitname as unit,shop.shopid as shopid from goods,shop,unit,class where goods.shopid=shop.shopid and goods.classid=class.classid and class.unitid=unit.unitid where goodsname like '%"+goodsName+"%' and shop.shopid!='"+currentShopID+"'";
+		 
+		 
+		return toJSON(super.quryBySql2(sql));
 		
-		
-		return null;
 	}
 
 }
